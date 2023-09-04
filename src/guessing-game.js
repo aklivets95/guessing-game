ba402822273e062f05dcb596d834b1364f0ed128
aklivets95 <sakraynev95@yandex.ru>
2023-09-04 19:15:45 +0300

    done: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minNumber = 0;
+        this.maxNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.minNumber = min;
+        this.maxNumber = max;
     }
 
     guess() {
-
+        this.middleNumber = Math.round((this.maxNumber + this.minNumber) / 2);
+        return this.middleNumber;
     }
 
     lower() {
-
+        this.maxNumber = this.middleNumber;
+        return this.maxNumber;
     }
 
     greater() {
-
+        this.minNumber = this.middleNumber;
+        return this.minNumber;
     }
 }
 
